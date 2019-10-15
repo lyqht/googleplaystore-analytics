3b9020040aa9766761314e581eeda7036882694d
lyqht <estee.tsw@gmail.com>
2019-10-15 16:51:10 +0800

    changed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 echo "\
-[general]\n\
-email=\"estee.tsw@gmail.com\"\n\
-" >~/.streamlit/credentials.toml
-
-echo "\
 [server]\n]
 enableCORS=false
 headless = true\n\
 port = $PORT\n\
-" > ~/.streamlit.config.toml
+" > ./.streamlit.config.toml
