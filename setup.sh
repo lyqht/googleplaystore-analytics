de963fb2567bbc41e98f4a08be4b619e59735b9b
lyqht <estee.tsw@gmail.com>
2019-10-16 15:22:12 +0800

    changed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
-enableCORS=false
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-" > ./.streamlit.config.toml
-
-echo $PORT
+" > ~/.streamlit/config.toml
