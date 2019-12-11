005558b6cc4041d967f7919c369a0a63e9cd413b
wanyun1903 <wanyun1903@users.noreply.github.com>
2019-12-11 15:21:24 +0800

    Update references.py

--- website/references.py
+++ website/references.py
@@ -8,5 +8,6 @@ def write():
              More on Dirichlet Distribution: https://towardsdatascience.com/dirichlet-distribution-a82ab942a879
              Image of LDA: https://www.depends-on-the-definition.com/understanding-text-data-with-topic-models/
              Image of DecisionTree: https://forums.tumult.com/t/animating-decision-trees/13870
+             Image of SVM: https://towardsdatascience.com/support-vector-machines-for-classification-fc7c1565e3
 
              """)
