c35670a57af045398293d529fb66aa03a5ba6de0
wanyun1903 <wanyun1903@users.noreply.github.com>
2019-12-11 14:55:10 +0800

    Update references.py

--- website/references.py
+++ website/references.py
@@ -4,8 +4,9 @@ import streamlit as st
 def write():
     st.title("Acknowledgements")
     st.write("""
-             Image of Dirichlet Distributions: https://en.wikipedia.org/wiki/Dirichlet_distribution 
+             Image of Dirichlet Distributions: https://en.wikipedia.org/wiki/Dirichlet_distribution
              More on Dirichlet Distribution: https://towardsdatascience.com/dirichlet-distribution-a82ab942a879
              Image of LDA: https://www.depends-on-the-definition.com/understanding-text-data-with-topic-models/
-             
+             Image of DecisionTree: https://forums.tumult.com/t/animating-decision-trees/13870
+
              """)
